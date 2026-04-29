1b183a143f723f4cf7a29a439b6370ecdbc078fa
ruanzadaa17-bit <@ruanzadaa17@gmail.com>
2026-04-29 11:29:06 -0300

    Fizemos conceitos de javascript, html e css

--- AULAS-HTML/Atividade/js/scripts.js
+++ AULAS-HTML/Atividade/js/scripts.js
@@ -1,5 +1,4 @@
-let userCount
-
+let userCount = 0; // Contador
 
 const cadastrar = (event) => {
   // Parar a propagação padrão do evento
@@ -10,29 +9,43 @@ const cadastrar = (event) => {
   // Capturando o elemento de lista por Id
   let lista = document.getElementById("lista");
 
-  userCount++;
-  let id = userCount;
-  // Criar um novo elemento <li>Nome - Email</li>
+  userCount++; // Acrescer o contador em
+  let id = userCount; // Criar um novo elemento <li>Nome - Email</li>
   let novoItem = document.createElement("li");
-  novoItem.innerText = `${id} - $`{nome} - ${email} <p onClick=editar(${id})class='btn'>Editar</p> <p onClick=excluir(${id}) class='btn'>Excluir</p>';
+  novoItem.innerHTML = `${id} - ${nome} - ${email} - <p onClick=editar(${id}) class='btn'>Editar</p> - <p onClick=excluir(${id}) class='btn'>Excluir</p>`;
+
   // Adicionar o novo item na lista ja existente
   lista.appendChild(novoItem);
+
   // Limpar os campos
   document.getElementById("nome").value = "";
   document.getElementById("email").value = "";
+};
 
-   editar = (id) =>
-
-const excluir = (id) => {
-
-const lista = document.getElementById("lista")
-const lista = document.getElementByTagName("li")
-
-itens.forEach((item) => {
+const editar = (id) => {
+  const lista = document.getElementById("lista");
+  const itens = document.querySelectorAll("li");
+  itens.forEach((item) => {
     if (item.innerHTML.includes(id)) {
-        item.remove();
-    }
-})
-}
+      // recuperar o texto do item
+      // cortar a string e variaveis nome e email
+      let nome = item.innerHTML.split(" - ")[3];
+      let email = item.innerHTML.split(" - ")[2];
 
+      document.getElementById("nome").value = nome;
+      document.getElementById("email").value = email;
+    }
+  });
+};
+
+// Função Escluir
+const excluir = (id) => {
+  const lista = document.getElementById("lista");
+  const itens = document.querySelectorAll("li");
+
+  itens.forEach((item) => {
+    if (item.innerHTML.includes(id)) {
+      item.remove();
+    }
+  });
 };
